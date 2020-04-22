663b31f54c0ba087ca903602eb5de90ac65acb71
TanishqSingla <tanishqsingla00@gmail.com>
2020-04-22 23:33:05 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.deepOrange[400],
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[],
+          ),
+        ),
+      ),
+    );
+  }
+}
