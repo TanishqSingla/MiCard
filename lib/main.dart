6051895539d01596b2a6d7f9ca41cb205197b689
TanishqSingla <tanishqsingla00@gmail.com>
2020-04-22 23:53:13 +0530

    Working on About
    Started working on about section

--- lib/main.dart
+++ lib/main.dart
@@ -9,33 +9,35 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.deepOrange[400],
         body: SafeArea(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                CircleAvatar(
-                  radius: 50.0,
-                  backgroundImage: NetworkImage(
-                      'https://avatars2.githubusercontent.com/u/26207583?s=460&u=d99eb73bfb2fcd6eead3a4fc54e68919390cc273&v=4'),
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: NetworkImage(
+                    'https://avatars2.githubusercontent.com/u/26207583?s=460&u=d99eb73bfb2fcd6eead3a4fc54e68919390cc273&v=4'),
+              ),
+              Text(
+                'Tanishq Singla',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontFamily: 'Lobster',
                 ),
-                Text(
-                  'Tanishq Singla',
-                  style: TextStyle(
-                    fontSize: 40.0,
-                    color: Colors.white,
-                    fontFamily: 'Lobster',
-                  ),
+              ),
+              Text(
+                'JS DEVELOPER',
+                style: TextStyle(
+                    letterSpacing: 2.5,
+                    fontSize: 20.0,
+                    fontFamily: 'Source Sans Pro',
+                    color: Colors.deepOrange[200]),
+              ),
+              Container(
+                child: Row(
+                  children: <Widget>[],
                 ),
-                Text(
-                  'JS DEVELOPER',
-                  style: TextStyle(
-                      letterSpacing: 2.5,
-                      fontSize: 20.0,
-                      fontFamily: 'Source Sans Pro',
-                      color: Colors.deepOrange[200]),
-                )
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
