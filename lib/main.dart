16f2489a27a876008e05de5999a6fba6be33d853
TanishqSingla <tanishqsingla00@gmail.com>
2020-04-22 23:51:20 +0530

    Added content
    Added a circle avatar
    Added a Name text and an about text

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,33 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.deepOrange[400],
         body: SafeArea(
-          child: Column(
-            children: <Widget>[],
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: NetworkImage(
+                      'https://avatars2.githubusercontent.com/u/26207583?s=460&u=d99eb73bfb2fcd6eead3a4fc54e68919390cc273&v=4'),
+                ),
+                Text(
+                  'Tanishq Singla',
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontFamily: 'Lobster',
+                  ),
+                ),
+                Text(
+                  'JS DEVELOPER',
+                  style: TextStyle(
+                      letterSpacing: 2.5,
+                      fontSize: 20.0,
+                      fontFamily: 'Source Sans Pro',
+                      color: Colors.deepOrange[200]),
+                )
+              ],
+            ),
           ),
         ),
       ),
