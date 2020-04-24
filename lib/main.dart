4802b6a0386204f3d3bd794423f8799d08b97fd1
TanishqSingla <tanishqsingla00@gmail.com>
2020-04-24 16:16:49 +0530

    Added custom cards

--- lib/main.dart
+++ lib/main.dart
@@ -30,11 +30,53 @@ class MyApp extends StatelessWidget {
                     letterSpacing: 2.5,
                     fontSize: 20.0,
                     fontFamily: 'Source Sans Pro',
+                    fontWeight: FontWeight.bold,
                     color: Colors.deepOrange[200]),
               ),
               Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                padding: EdgeInsets.all(10.0),
                 child: Row(
-                  children: <Widget>[],
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.deepOrange[800],
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      '+91 1239491234',
+                      style: TextStyle(
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 20.0,
+                          color: Colors.deepOrange[800]),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.deepOrange[800],
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'tanishq@gmail.com',
+                      style: TextStyle(
+                          color: Colors.deepOrange[800],
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 20.0),
+                    )
+                  ],
                 ),
               ),
             ],
